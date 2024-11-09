4f6d847bedb8349a028063b770e05e9800fafaf3
Yannick Yemo <yemo.yannick@hotmail.com>
2024-11-08 17:53:43 -0800

    Project - Car Rental
    Adding files for the car rental project in python

---
+++ car_rental_list.py
@@ -0,0 +1,14 @@
+======================
+= Welcome To YemoCar =
+======================
+
+Please make your chose to our rental car catalogue:
+
+- Hyundai => $10/h | $15/day | $20/week
+- Toyota => $20/h | $25/day | $30/week
+- Kia => $30/h | $35/day | $40/week
+- Honda => $40/h | $45/day | $50/week
+- Ford => $50/h | $55/day | $60/week
+- Chevrolet => $60/h | $65/day | $70/week
+- Cadillac => $70/h | $75/day | $80/week
+- Audi => $80/h | $85/day | $90/week
